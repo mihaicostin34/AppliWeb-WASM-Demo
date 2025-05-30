b7ede8fc5a8ae9113d08ab2eaf652fdc6203c37d
AstridG <95365617+stridouille@users.noreply.github.com>
2025-05-30 20:34:12 +0000

    autres petites routes ultra kiki

--- serveur/src/main/java/web/projet/serveur/Controller.java
+++ serveur/src/main/java/web/projet/serveur/Controller.java
@@ -1,5 +1,6 @@
 package web.projet.serveur;
 
+import java.sql.Date;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,19 +46,6 @@ public class Controller {
         return computerRepository.findAll();
     }
     
-    @PostMapping("/computer")
-    public Computer createComputer(@RequestParam String hostname, @RequestParam String ip, 
-    @RequestParam Integer maxRam, @RequestParam Integer maxCpu, 
-    @RequestParam Integer maxStorage) {
-        Computer computer = new Computer();
-        computer.setHostname(hostname);
-        computer.setIp(ip);
-        computer.setMaxRam(maxRam);
-        computer.setMaxCpu(maxCpu);
-        computer.setMaxStorage(maxStorage);
-        return computerRepository.save(computer);
-    }
-
     @GetMapping("/computer")
     public Computer getComputer(@RequestParam Long id) {
         return computerRepository.findById(id).orElseThrow(() -> new RuntimeException("Computer not found"));
@@ -74,18 +62,23 @@ public class Controller {
         Computer computer = computerRepository.findById(id).orElseThrow(() -> new RuntimeException("Computer not found"));
         return computer.getStorages();
     }
+
+    @PostMapping("/computer")
+    public Computer createComputer(@RequestParam String hostname, @RequestParam String ip, 
+    @RequestParam Integer maxRam, @RequestParam Integer maxCpu, 
+    @RequestParam Integer maxStorage) {
+        Computer computer = new Computer();
+        computer.setHostname(hostname);
+        computer.setIp(ip);
+        computer.setMaxRam(maxRam);
+        computer.setMaxCpu(maxCpu);
+        computer.setMaxStorage(maxStorage);
+        return computerRepository.save(computer);
+    }
     
     @GetMapping("/groups")
     public Collection<Group> getGroups() {
         return groupRepository.findAll();
-    }
-
-    @PostMapping("/group")
-    public Group createGroup(@RequestParam String name, @RequestParam String description) {
-        Group group = new Group();
-        group.setName(name);
-        group.setDescription(description);
-        return groupRepository.save(group);
     }
     
     @GetMapping("/group")
@@ -97,6 +90,14 @@ public class Controller {
     public Collection<Membership> getGroupMemberships(@RequestParam Long id) {
         Group group = groupRepository.findById(id).orElseThrow(() -> new RuntimeException("Group not found"));
         return group.getMemberships();
+    }
+
+    @PostMapping("/group")
+    public Group createGroup(@RequestParam String name, @RequestParam String description) {
+        Group group = new Group();
+        group.setName(name);
+        group.setDescription(description);
+        return groupRepository.save(group);
     }
 
     @GetMapping("/logs")
@@ -137,6 +138,17 @@ public class Controller {
         return processRepository.findAll();
     }
 
+    @GetMapping("/process")
+    public Process getProcess(@RequestParam Long id) {
+        return processRepository.findById(id).orElseThrow(() -> new RuntimeException("Process not found"));
+    }
+
+    @GetMapping("/process/resourceUsages")
+    public Collection<ResourceUsage> getProcessResourceUsages(@RequestParam Long id) {
+        Process process = processRepository.findById(id).orElseThrow(() -> new RuntimeException("Process not found"));
+        return process.getResourceUsages();
+    }
+
     @PostMapping("/process")
     public Process createProcess(@RequestParam String name, @RequestParam Integer pid, 
                                  @RequestParam Long resourceUsageId, @RequestParam Long sessionId) {
@@ -148,10 +160,101 @@ public class Controller {
         return processRepository.save(process);
     }
 
+    @PostMapping("/process/resourceUsage")
+    public ResourceUsage createResourceUsage(@RequestParam Long processId, @RequestParam long time, 
+                                             @RequestParam double usedRam, @RequestParam double usedCpu) {
+        Process process = processRepository.findById(processId).orElseThrow(() -> new RuntimeException("Process not found"));
+
+        ResourceUsage resourceUsage = new ResourceUsage();
+        resourceUsage.setTime(time);
+        resourceUsage.setUsedRam(usedRam);
+        resourceUsage.setUsedCpu(usedCpu);
+        resourceUsage.setResourceUser(process);
+
+        return resourceUsageRepository.save(resourceUsage);
+    }
+
+    @GetMapping("/resourceUsages")
+    public Collection<ResourceUsage> getResourceUsages() {
+        return resourceUsageRepository.findAll();
+    }
+
+    @GetMapping("/resourceUsage")
+    public ResourceUsage getResourceUsage(@RequestParam Long id) {
+        return resourceUsageRepository.findById(id).orElseThrow(() -> new RuntimeException("Resource Usage not found"));
+    }
+
+    @GetMapping("/sessions")
+    public Collection<Session> getSessions() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping("/session")
+    public Session getSession(@RequestParam Long id) {
+        return sessionRepository.findById(id).orElseThrow(() -> new RuntimeException("Session not found"));
+    }
+
+    @PostMapping("/session")
+    public Session createSession(@RequestParam Long computerId, 
+                                 @RequestParam Long userId, @RequestParam Date startTime) {
+        Computer computer = computerRepository.findById(computerId).orElseThrow(() -> new RuntimeException("Computer not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        Session session = new Session();
+        session.setStartTime(startTime);
+        session.setComputer(computer);
+        session.setUser(user);
+
+        return sessionRepository.save(session);
+    }
+
+    @GetMapping("/storages")
+    public Collection<Storage> getStorages() {
+        return storageRepository.findAll();
+    }
+
+    @PostMapping("/storage")
+    public Storage createStorage(@RequestParam Long userId, 
+                                 @RequestParam Long computerId,
+                                 @RequestParam Integer storageUsed) {
+        Computer computer = computerRepository.findById(computerId).orElseThrow(() -> new RuntimeException("Computer not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        Storage storage = new Storage();
+        storage.setUser(user);
+        storage.setComputer(computer);
+        storage.setStorageUsed(storageUsed);
+
+        return storageRepository.save(storage);
+    }
+
 
     @GetMapping("/users")
     public Collection<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @GetMapping("/user/sessions")
+    public Collection<Session> getUserSessions(@RequestParam Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getSessions();
+    }
+
+    @GetMapping("/user/storages")
+    public Collection<Storage> getUserStorages(@RequestParam Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getStorages();
+    }
+
+    @GetMapping("/user/memberships")
+    public Collection<Membership> getUserMemberships(@RequestParam Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getMemberships();
     }
 
     @PostMapping("/user")
@@ -162,18 +265,7 @@ public class Controller {
         return userRepository.save(user);   
     }
 
-    @GetMapping("/user")
-    public User getUser(@RequestParam Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
-    }
 
-    @GetMapping("/user/memberships")
-    public Collection<Membership> getUserMemberships(@RequestParam Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
-        return user.getMemberships();
-    }
-
-    
 
     
 
